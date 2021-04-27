4ef4c1426036939dfbddd2eb66a012ce55093a6e
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-04-27 17:10:03 +0100

    refactor: code cleaning

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,18 +1,19 @@
 import ExpenseItem from './ExpenseItem';
 
+import './ExpensesList.css';
+
 const ExpensesList = ({expenses}) => {
-  let expensesContent = <p>No content to display</p>
-  if (expenses.length > 0) {
-    expensesContent = expenses.map(expense => {
-        return <ExpenseItem
-          key={expense.id}
-          date={expense.date}
-          title={expense.title}
-          amount={expense.amount}
-        />
-      }
-    )}
-  return <div>{expensesContent}</div>
+  if (expenses.length === 0)
+    return <h2 className='expenses-list__fallback'>No Expenses Found</h2>
+
+  return <ul className='expenses-list'>{
+    expenses.map(expense => <ExpenseItem
+      key={expense.id}
+      date={expense.date}
+      title={expense.title}
+      amount={expense.amount}
+    />)
+  }</ul>
 }
 
 export default ExpensesList;
