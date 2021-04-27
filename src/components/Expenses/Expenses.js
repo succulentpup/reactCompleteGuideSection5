8094e73166c4b430b9606f01cfa6025e0ee9d4b0
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-04-27 16:34:15 +0100

    implemented filtering expenses by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,21 +8,24 @@ import './Expenses.css';
 
 const Expenses = ({expenses}) => {
   const [filterYear, setFilterYear] = useState('2022')
-  const filterChangeHandler = (year) => {
-    console.log({filter: year}, 'filter');
-    setFilterYear(year)
-  }
+  const filterChangeHandler = (year) => setFilterYear(year);
+  const filteredExpenses = expenses.filter(expense =>
+    expense.date.getFullYear().toString() === filterYear);
 
   return (
     <Card className='expenses'>
       <ExpensesFilter year={filterYear} onFilterChange={filterChangeHandler}/>
-      {expenses.map(expense => <ExpenseItem
-          key = {expense.id}
-          date = { expense.date }
-          title = { expense.title }
-          amount = { expense.amount }
-        />
-      )}
+      <div>
+        {filteredExpenses.map(expense => {
+          return <ExpenseItem
+              key={expense.id}
+              date={expense.date}
+              title={expense.title}
+              amount={expense.amount}
+            />
+          }
+        )}
+      </div>
     </Card>
   );
 }
